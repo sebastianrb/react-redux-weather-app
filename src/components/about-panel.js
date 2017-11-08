f75faba8646fca4151e9ff921a47621c67dbf4d5
Sebastian <sebastian.r.rb@gmail.com>
2017-11-08 14:26:31 -0500

    Remove max width

--- src/components/about-panel.js
+++ src/components/about-panel.js
@@ -1,24 +1,32 @@
 import React from "react";
-import EasyTransition from 'react-easy-transition';
+import EasyTransition from "react-easy-transition";
 import { Link } from "react-router-dom";
 import { transitionSetting } from "../index.js";
 
-
-const AboutPanel = (props) => {
+const AboutPanel = props => {
     return (
         <EasyTransition
             path={location.pathname}
-            initialStyle={{opacity: 0, transform: "scale(.9)"}}
+            initialStyle={{ opacity: 0, transform: "scale(.9)" }}
             transition={transitionSetting}
-            finalStyle={{opacity: 1, transform: "scale(1)"}}
+            finalStyle={{ opacity: 1, transform: "scale(1)" }}
         >
             <div className="about-panel">
                 <h2 className="about-panel__main-heading">About the Weather App</h2>
-                <p className="about-panel__description">This weather application utilizes the React.js UI framework in conjunction with the Redux state container. The application connects to the Yahoo Weather API and the Open Weather Map API to collect weather data and saves that information to a Redux data store. The various components are then built out dynamically based on the city and various weather characteristics such as temperature and conditions.</p>
-                <p className="about-panel__description">This is a single page application; the sectional partitioning of the site is implemented using React Routing.</p>
+                <p className="about-panel__description">
+                    This weather application utilizes the React.js UI framework in conjunction with the Redux state
+                    container. The application connects to the Yahoo Weather API and the Open Weather Map API to collect
+                    weather data and saves that information to a Redux data store. The various components are then built
+                    out dynamically based on the city and various weather characteristics such as temperature and
+                    conditions.
+                </p>
+                <p className="about-panel__description">
+                    This is a single page application; the sectional partitioning of the site is implemented using React
+                    Router.
+                </p>
             </div>
         </EasyTransition>
     );
-}
+};
 
 export default AboutPanel;
